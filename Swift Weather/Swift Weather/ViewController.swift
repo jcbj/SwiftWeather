750f0c72a14e2b86da9e7fd7b0bb3bafb2062f46
jiangchao <jiangchaobj@yahoo.com>
2015-11-06 22:22:03 +0800

    快速构建原型完成，顺带测试Commit命令

--- Swift Weather/Swift Weather/ViewController.swift
+++ Swift Weather/Swift Weather/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var location: UILabel!
+    @IBOutlet weak var imageWeather: UIImageView!
+    @IBOutlet weak var temperature: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let background = UIImage(named: "background.png")
+        self.view.backgroundColor = UIColor(patternImage: background!)
     }
 
     override func didReceiveMemoryWarning() {
